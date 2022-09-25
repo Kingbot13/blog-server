731d0d21432bad95d5a022a222c06716e625a540
Dylan King <dwkblue51@yahoo.com>
2022-09-25 09:42:26 -0500

    add route for updating posts

--- routes/api.js
+++ routes/api.js
@@ -14,4 +14,7 @@ router.post("/posts/create-posts", apiController.postCreatePost);
 // handle create comment
 router.post("/posts/:id/create-comment", apiController.commentCreatePost);
 
+// handle post update
+router.post("/posts/:id/update", apiController.postUpdatePost);
+
 module.exports = router;
